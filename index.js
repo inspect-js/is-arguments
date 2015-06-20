1e59f2bf79454188145de5275a64996eafc94420
Jordan Harband <ljharb@gmail.com>
2015-06-20 12:54:06 -0700

    `toString` as a variable name breaks in some older browsers.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var toString = Object.prototype.toString;
+var toStr = Object.prototype.toString;
 
 module.exports = function isArguments(value) {
-	var str = toString.call(value);
+	var str = toStr.call(value);
 	var isArgs = str === '[object Arguments]';
 	if (!isArgs) {
 		isArgs = str !== '[object Array]' &&
@@ -9,7 +9,7 @@ module.exports = function isArguments(value) {
 			typeof value === 'object' &&
 			typeof value.length === 'number' &&
 			value.length >= 0 &&
-			toString.call(value.callee) === '[object Function]';
+			toStr.call(value.callee) === '[object Function]';
 	}
 	return isArgs;
 };
